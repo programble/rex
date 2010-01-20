be0d0c2042e52b472f8c9e2030c915376c0ad4be
Curtis (Programble) <programble@gmail.com>
2010-01-20 16:54:18 -0500

    Initial Commit

---
+++ rex.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       rex.py
+#       
+#       Copyright 2010 Curtis (Programble) <programble@gmail.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+__version__ = "0.1.0"
+
+from optparse import OptionParser
+
+def version_info():
+    # GNU Coding guidelines suggests:
+    # GNU hello 2.3
+    # Copyright (C) 2007 Free Software Foundation, Inc.
+    # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+    # This is free software: you are free to change and redistribute it.
+    # There is NO WARRANTY, to the extent permitted by law.
+    print "Rex", __version__
+    print "Copyright (C) 2010 Curtis (Programble) <programble@gmail.com>"
+    print "License GPLv3: GNU GPL version 3 <http://gnu.org/licenses/gpl.html>"
+    print "This is free software: you are free to change and redistribute it."
+    print "There is NO WARRANTY, to the extent permitted by law."
+
+def main():
+	
+	return 0
+
+if __name__ == '__main__':
+	main()
